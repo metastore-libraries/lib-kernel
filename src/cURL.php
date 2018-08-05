175bb82ff7517ea7a1b68309c88fc40a746fb32f
Kitsune Solar <kitsune.solar@gmail.com>
2018-08-05 04:58:05 +0300

    2018-08-05 01:58:05

--- src/cURL.php
+++ src/cURL.php
@@ -21,9 +21,9 @@ class cURL {
 
 		curl_setopt_array( $ch, [
 			CURLOPT_URL            => $url,
-			CURLOPT_POST           => false,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_POST           => 0,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_SSL_VERIFYPEER => 0,
 			CURLOPT_USERAGENT      => 'Mozilla/5.0',
 			CURLOPT_HTTPHEADER     => $headers,
 		] );
@@ -38,21 +38,22 @@ class cURL {
 	 * cURL: Get JSON.
 	 *
 	 * @param $url
+	 * @param string $headers
 	 * @param int $cache
 	 * @param float|int $time
 	 *
 	 * @return mixed|string
 	 */
-	public static function getJSON( $url, $cache = 1, $time = 60 * 60 ) {
+	public static function getJSON( $url, $headers = '', $cache = 1, $time = 60 * 60 ) {
 		$cachePath = Route::DOCUMENT_ROOT() . 'storage/cache/';
 		$cacheFile = $cachePath . Hash::get( 'md5', $url );
 		$refresh   = $time;
 
 		if ( ( ( $refresh ) < ( time() - filectime( $cacheFile ) ) || filesize( $cacheFile ) == 0 ) || ! $cache ) {
-			$out = Parser::json( self::getURL( $url ) );
+			$out = Parser::json( self::getURL( $url, $headers ) );
 
 			$handle = fopen( $cacheFile, 'wb' ) or die( 'no fopen' );
-			$json_cache = self::getURL( $url );
+			$json_cache = self::getURL( $url, $headers );
 
 			fwrite( $handle, $json_cache );
 			fclose( $handle );
