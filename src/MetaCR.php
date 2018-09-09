6fec041a3ad66d0761170a6a8c28c6cd85e2642c
Kitsune Solar <kitsune.solar@gmail.com>
2018-09-09 16:45:46 +0300

    2018-09-09 13:45:46

--- src/MetaCR.php
+++ src/MetaCR.php
@@ -14,7 +14,7 @@ class MetaCR {
 	 * @return mixed
 	 */
 	public static function getCR() {
-		$cr  = 'PHNwYW4gY2xhc3M9Im1ldGEtY3IiIHN0eWxlPSJ0ZXh0LXRyYW5zZm9ybTogdXBwZXJjYXNlOyI+TWFkZSB3aXRoIDxzcGFuIGNsYXNzPSJmYXMgZmEtaGVhcnQiIHN0eWxlPSJjb2xvcjogI2ZlNmUzYTsiPjwvc3Bhbj4gYnkgPGEgdGl0bGU9IldlYiBBcHBsaWNhdGlvbiBEZXZlbG9wbWVudCBDb21wYW55IiBocmVmPSJodHRwczovL21ldGFkYXRhLmZvdW5kYXRpb24vIj48c3Ryb25nPk1FVEFEQVRBPC9zdHJvbmc+PC9hPjwvc3Bhbj4=';
+		$cr  = 'PHNwYW4gY2xhc3M9Im1ldGEtY3IiPjxpIGNsYXNzPSJmYXMgZmEtY29kZSI+PC9pPiB3aXRoIDxpIGNsYXNzPSJmYXMgZmEtaGVhcnQiIHN0eWxlPSJjb2xvcjogI2ZlNmUzYTsiPjwvaT4gYnkgPGEgdGl0bGU9IldlYiBBcHBsaWNhdGlvbiBEZXZlbG9wbWVudCBDb21wYW55IiBocmVmPSJodHRwczovL21ldGFkYXRhLmZvdW5kYXRpb24vIj48c3Ryb25nPk1FVEFEQVRBPC9zdHJvbmc+PC9hPjwvc3Bhbj4=';
 		$out = Hash::base64( $cr );
 
 		return $out;
